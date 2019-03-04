26a10f7ed367a3fc6b5247f6a1b4cc486f3f9ee5
Max Kunzelmann <maxdev@posteo.de>
2019-03-04 13:14:21 +0100

    Create .config/nvim folder before writing to init.vim

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,7 @@ if [ ! -f ~/.local/share/nvim/site/autoload/plug.vim ]; then
   curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim 
 fi
+mkdir ~/.config/nvim/
 printf "so $HOME/.dotfiles/.config/nvim/init.vim" > ~/.config/nvim/init.vim
 
 # Tmux

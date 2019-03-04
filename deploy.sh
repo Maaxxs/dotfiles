ea185e51ddd9ed76a77128208f6d3f216935a1a2
Max Kunzelmann <maxdev@posteo.de>
2019-03-04 07:16:59 +0100

    Fix tmux path issue

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ mv ~/.config/nvim/init.vim ~/.config/nvim/init.vim.old
 
 printf "source $HOME/.dotfiles/.zshrc" > ~/.zshrc
 printf "so $HOME/.dotfiles/.config/nvim/init.vim" > ~/.config/nvim/init.vim
-printf "source-file $HOME/.dotfiles/tmux/tmux.conf" > ~/.tmux.conf
+printf "source-file $HOME/.dotfiles/.tmux.conf" > ~/.tmux.conf
 printf "source $HOME/.dotfiles/.oh-my-zsh/custom/aliases.zsh" > ~/.oh-my-zsh/custom/aliases.zsh
 printf "source $HOME/.dotfiles/.oh-my-zsh/custom/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh" > ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
 

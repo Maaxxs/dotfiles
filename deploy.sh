a84bacd8680aad71910ac106d637accd6d3fd7c2
Max Kunzelmann <maxdev@posteo.de>
2019-03-04 11:42:48 +0100

    Add installation of vim plug for NeoVim

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,20 @@ mv ~/.tmux.conf ~/.tmux.conf.old
 mv ~/.config/nvim/init.vim ~/.config/nvim/init.vim.old
 
 
+# Put the zsh sources in place
 printf "source $HOME/.dotfiles/.zshrc" > ~/.zshrc
-printf "so $HOME/.dotfiles/.config/nvim/init.vim" > ~/.config/nvim/init.vim
-printf "source-file $HOME/.dotfiles/.tmux.conf" > ~/.tmux.conf
 printf "source $HOME/.dotfiles/.oh-my-zsh/custom/aliases.zsh" > ~/.oh-my-zsh/custom/aliases.zsh
 printf "source $HOME/.dotfiles/.oh-my-zsh/custom/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh" > ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+
+# NeoVim
+if [ ! -f ~/.local/share/nvim/site/autoload/plug.vim ]; then
+  curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim 
+fi
+printf "so $HOME/.dotfiles/.config/nvim/init.vim" > ~/.config/nvim/init.vim
+
+# Tmux
+printf "source-file $HOME/.dotfiles/.tmux.conf" > ~/.tmux.conf
 
 
 

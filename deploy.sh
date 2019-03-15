4e49c4ac022e2c0da28e8ca4d713f2498f8ab640
Max Kunzelmann <maxdev@posteo.de>
2019-03-15 20:13:37 +0100

    Change default shell to zsh.
    Create routine to check for configuration files and create a backup of
    the file it it exists.

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,30 @@
-mv ~/.zshrc ~/.zshrc.old
-mv ~/.tmux.conf ~/.tmux.conf.old
-mv ~/.config/nvim/init.vim ~/.config/nvim/init.vim.old
+#!/bin/bash
 
+# Create backup of configuration files if they exist
+configuration_files=(
+    "~/.zshrc"
+    "~/.tmux.conf"
+    "~/.config/nvim/init.vim"
+    )
+
+for file in ${configuration_files[@]}
+do
+    if [ -f $file ]
+    then
+        mv $file "$file.old"
+    fi
+done
 
 # Put the zsh sources in place
 printf "source $HOME/.dotfiles/.zshrc" > ~/.zshrc
 printf "source $HOME/.dotfiles/.oh-my-zsh/custom/aliases.zsh" > ~/.oh-my-zsh/custom/aliases.zsh
 printf "source $HOME/.dotfiles/.oh-my-zsh/custom/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh" > ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
 
-# NeoVim
-if [ ! -f ~/.local/share/nvim/site/autoload/plug.vim ]; then
-  curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim 
+# Load VimPlug at start of NeoVim
+if [ ! -f ~/.local/share/nvim/site/autoload/plug.vim ]
+then
+    curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim 
 fi
 mkdir -p ~/.config/nvim/
 printf "so $HOME/.dotfiles/.config/nvim/init.vim" > ~/.config/nvim/init.vim
@@ -19,5 +32,7 @@ printf "so $HOME/.dotfiles/.config/nvim/init.vim" > ~/.config/nvim/init.vim
 # Tmux
 printf "source-file $HOME/.dotfiles/.tmux.conf" > ~/.tmux.conf
 
-
+# change default shell to zsh
+echo '[*] Changing shell to zsh'
+sudo chsh -s $(which zsh) $USER
 

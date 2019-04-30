049f210b50b265c6a0e4f4c6073ed15d12bddde4
Max Kunzelmann <maxdev@posteo.de>
2019-04-30 11:31:30 +0200

    System update shortcuts are adaptive
    - Ubuntu: apt is used
    - Arch: trizen is used

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -5,25 +5,30 @@ alias sudo='sudo '
 # requires package 'most'
 alias man='man -P most'
 
+# Updating system
+if command -v trizen > /dev/null;
+then
+    alias up='trizen -Syyu'
+    alias s='trizen -Ss'        
+    alias si='trizen -Si'
+    alias i='trizen -S'        
+    alias u='trizen -Runs'
+    alias q='trizen -Qi'
+    alias pacs='pacman -Ss'
+elif command -v apt > /dev/null;
+then
+    alias up='sudo apt update && sudo apt dist-upgrade'
+    alias s='apt search'        
+    alias si='apt show'
+    alias i='sudo apt install'        
+    alias u='sudo apt purge && sudo apt autoremove'
+    alias q='apt-cache show'
+fi
+
 
 # neovim
 alias nvimconfig='nvim ~/.config/nvim/init.vim'
 alias v='nvim'
-
-# Updating system
-alias up='trizen -Syyu'
-alias s='trizen -Ss'        
-alias si='trizen -Si'
-alias i='trizen -S'        
-alias u='trizen -Runs'
-alias q='trizen -Qi'
-alias pacs='pacman -Ss'
-
-# System update for Elementary OS
-alias eup='sudo apt update && sudo apt full-upgrade'
-alias es='apt search'
-alias esi='apt show'
-alias ei='sudo apt install'
 
 
 # grep

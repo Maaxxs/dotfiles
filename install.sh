8ca1fde70beaefbd50b6ecdc9eaaf329b4236fd9
Max Kunzelmann <maxdev@posteo.de>
2020-04-05 19:55:20 +0200

    Installs now the pateched Source Code Pro by NerdFont
    - install util-linux-user for fedora installations to provide the
    command `chsh`

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+packages=()
+
 # Looking for package manager
 if command -v pacman 1>/dev/null; then
   installer='pacman -Sy'
@@ -9,11 +11,12 @@ elif command -v apt 1>/dev/null; then
   installer='apt install'
 elif command -v dnf 1>/dev/null; then
   installer='dnf install'
+  # This package provides `chsh` on fedora
+  packages+=('util-linux-user')
 elif command -v eopkg 1>/dev/null; then
   installer='eopkg install'
 fi
 
-packages=()
 
 if ! command -v git 1>/dev/null; then
   packages+=('git')
@@ -41,7 +44,7 @@ fi
 
 if [ ${#packages[@]} -gt 0 ]; then
   sudo $installer ${packages[@]}
-else 
+else
   echo 'Needed packages are already installed.'
 fi
 
@@ -54,9 +57,19 @@ if [ ! -d ~/.oh-my-zsh ]; then
 elif [ ! -d ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions/ ]; then
   echo 'Downloading zsh-autosuggestions'
   git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-else 
+else
   echo 'oh-my-zsh and zsh-autosuggestions are already there'
 fi
 
-
-
+# Install patched font "Source Code Pro"
+echo "Downloading Source Code Pro. This might take a while"
+mkdir -p $HOME/.local/share/fonts/SourceCodePro
+cd $HOME/.local/share/fonts/SourceCodePro
+latest_version=$(curl -s https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest | grep "browser_download_url" | grep "SourceCodePro.zip" | awk '{ print $2 }' | tr -d \")
+wget --quiet $latest_version
+echo "Setting Source Code Pro as default monospace font"
+unzip SourceCodePro.zip
+fc-cache -f
+rm -f SourceCodePro.zip
+gsettings set org.gnome.desktop.interface monospace-font-name 'SauceCodePro Nerd Font 10'
+cd

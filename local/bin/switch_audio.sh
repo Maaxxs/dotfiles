0d856712d56d6022f100e3e917b110581d48623c
Max Kunzelmann <maxdev@posteo.de>
2021-08-01 01:19:35 +0200

    Add audio switch script (mod+o). Tray is always on primary.
    Remove some comments

---
+++ local/bin/switch_audio.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+audio_devices=(
+    "alsa_output.pci-0000_00_1b.0.analog-stereo"
+    "alsa_output.usb-Logitech_Logitech_G430_Gaming_Headset-00.analog-stereo"
+)
+
+current_audio=$(pactl get-default-sink)
+for index in "${!audio_devices[@]}"; do
+    if [[ "${audio_devices[$index]}" = "$current_audio" ]]; then
+        current_audio_index=$index
+        break
+    fi
+done
+
+if [[ "$current_audio_index" = "" ]]; then 
+    pactl set-default-sink "${audio_devices[0]}"
+else
+    index=$(((index + 1) % 2))
+    pactl set-default-sink "${audio_devices[$index]}"
+fi
+
+
+
+

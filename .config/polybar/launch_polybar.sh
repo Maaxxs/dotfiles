a531b488fbe617647f488b332cecbc30e0afd5ff
Max Kunzelmann <maxdev@posteo.de>
2021-09-11 20:09:21 +0200

    Change volume steps to 5%. Change bar from it3status back to polybar

--- .config/polybar/launch_polybar.sh
+++ .config/polybar/launch_polybar.sh
@@ -1,12 +1,16 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 # Terminate already running bar instances
 killall -q polybar
 # If all your bars have ipc enabled, you can also use 
 # polybar-msg cmd quit
 
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
-polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
+# echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+# polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+# polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
 
-echo "Bars launched..."
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+polybar --reload bar1 & 
+polybar --reload bar2 & 

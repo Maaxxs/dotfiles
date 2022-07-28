f9f6fd58d508a0e8c9276c3a65c4ba3d411b8925
Max Kunzelmann <maxdev@posteo.de>
2022-07-28 14:25:33 +0200

    Add lock script with blur

---
+++ scripts/blur_lock.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e 
+trap cleanup HUP INT TERM
+
+function cleanup {
+    rm -f /tmp/lockscreen.png
+}
+
+
+grim -o $(swaymsg -t get_outputs | jq -r '.[] | select(.focused) | .name') /tmp/lockscreen.png
+convert -blur 0x8 /tmp/lockscreen.png /tmp/lockscreen.png
+convert -composite /tmp/lockscreen.png ~/Pictures/wallpapers/capslock-centered.png /tmp/lockscreen.png
+swaylock -Kfi /tmp/lockscreen.png
+cleanup
+

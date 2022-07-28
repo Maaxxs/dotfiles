a0a7f1063d47ae11bb459612d63ebde8d2fb5bf7
Max Kunzelmann <maxdev@posteo.de>
2022-07-28 14:26:50 +0200

    Add script to turn on/off laptop monitor.

---
+++ scripts/toggle_laptop_monitor.sh
@@ -0,0 +1,10 @@
+#/bin/sh
+
+ACTIVE=`swaymsg -t get_outputs | jq '.[] | select(.name == "eDP-1") | .active'`
+
+if $ACTIVE
+then
+    swaymsg 'output eDP-1 disable'
+else
+    swaymsg 'output eDP-1 enable'
+fi

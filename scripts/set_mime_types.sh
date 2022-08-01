0e278bb4c45698b83bafcc58713c3b37bafa004f
Max Kunzelmann <maxdev@posteo.de>
2022-08-01 23:24:47 +0200

    Add script to set mime types.

---
+++ scripts/set_mime_types.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# see list of mime types at /usr/share/applications/mimeinfo.cache
+
+xdg-mime default org.gnome.eog.desktop image/png
+xdg-mime default org.gnome.eog.desktop image/jpg
+xdg-mime default org.gnome.eog.desktop image/jpeg
+xdg-mime default org.gnome.eog.desktop image/gif
+xdg-mime default firefox.desktop x-scheme-handler/http
+xdg-mime default firefox.desktop x-scheme-handler/https
+
+

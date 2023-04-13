825c67e2f65b6f9e20cfc9ca77bb30db5d09b9c3
Max Kunzelmann <maxdev@posteo.de>
2023-04-13 11:43:34 -0600

    scripts: only show battery warning when not charging

--- scripts/.local/bin/low_battery_warning.sh
+++ scripts/.local/bin/low_battery_warning.sh
@@ -6,8 +6,9 @@ set -eu pipefail
 WARNING_PERC=10
 
 PERC=$(cat /sys/class/power_supply/BAT1/capacity)
+STATUS=$(cat /sys/class/power_supply/BAT1/status)
 
-if [ "$PERC" -le $WARNING_PERC ]; then
+if [ "$PERC" -le $WARNING_PERC ] && [ "$STATUS" != "Charging" ]; then
   notify-send -u critical "Low battery"
 fi
 

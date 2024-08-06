75d4db8e9c6247b2dea99975361e8ae284f1016a
Max Kunzelmann <maxdev@posteo.de>
2024-08-06 16:26:47 +0200

    fish: update configuration file

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,10 +1,32 @@
+set -g fish_greeting ""
+if status is-login
+    set TTY1 (tty)
+    [ "$TTY1" = /dev/tty1 ] && exec sway &>~/.Wsession.errors
+end
+
 # abbreviations
 abbr -a e nvim
 abbr -a g git
 abbr -a m make
 abbr -a o xdg-open
 abbr -a c cargo
-
+abbr -a lk 'lookup'
+abbr -a lka 'linank -a'
+abbr -a ta 'task add'
+abbr -a np "hugo new posts/(date +%Y-%m-%d)-"
+abbr -a tt 'taskwarrior-tui'
+abbr -a zs 'zola serve'
+abbr -a mov 'nvim movie-list.txt'
+abbr -a reading 'nvim reading-list.md'
+abbr -a todo 'nvim ~/nc/todo.txt'
+abbr -a ca 'calcurse'
+abbr -a irc 'senpai'
+abbr -a stb 'sudo systemctl start bluetooth'
+abbr -a hexedit 'hexedit -l16'
+abbr -a ymd 'date -u +%Y-%m-%d'
+abbr -a sb 'sudo systemctl start bluetooth'
+abbr -a hb 'sudo systemctl stop bluetooth'
+abbr -a ping9 'ping 9.9.9.9'
 
 alias hx="~/.cargo/bin/hx"
 alias hxconf="hx ~/.config/helix/config.toml"
@@ -12,28 +34,43 @@ alias rgi='rg -i'
 alias fishconf='nvim ~/.config/fish/config.fish'
 alias sshconf='nvim ~/.ssh/config'
 alias nvimconf='nvim ~/.config/nvim/init.lua'
+alias swayconf='nvim ~/.config/sway/config'
+alias muttconf='nvim ~/.config/neomutt/neomuttrc'
 alias dl4='yt-dlp -f mp4 -o "%(title)s.%(ext)s"'
 alias dl3='yt-dlp -f "ba" -x --audio-format mp3 -o "%(title)s.%(ext)s"'
 alias duh='du -md1 2>/dev/null | sort -g'
 alias digx='dig +short -x'
 alias tcplisten='netstat -ap tcp | grep LISTEN'
-alias cvpn='sudo systemctl start wg-quick@wg0'
-alias dvpn='sudo systemctl stop wg-quick@wg0'
+alias cvpn='sudo systemctl start wg-quick@wg0; kill -USR1 i3status'
+alias dvpn='sudo systemctl stop wg-quick@wg0; kill -USR1 i3status'
+alias chs='sudo systemctl start openvpn-client@hses'
+alias dhs='sudo systemctl stop openvpn-client@hses'
 alias cair='bluetoothctl connect (bluetoothctl devices | rg AirPods | cut -d" " -f2)'
 alias dair='bluetoothctl disconnect (bluetoothctl devices | rg AirPods | cut -d" " -f2)'
 alias csony='bluetoothctl connect (bluetoothctl devices | rg WH-1000XM3 | cut -d" " -f2)'
 alias dsony='bluetoothctl disconnect (bluetoothctl devices | rg WH-1000XM3 | cut -d" " -f2)'
+alias cbose='bluetoothctl connect (bluetoothctl devices | rg "Bose QC45" | cut -d" " -f2)'
+alias dbose='bluetoothctl disconnect (bluetoothctl devices | rg "Bose QC45" | cut -d" " -f2)'
+alias bld='bluetoothctl devices | cut -f2 -d" " | while read uuid; bluetoothctl info $uuid; end|grep -e "Device\|Connected\|Name"'
 alias cls="clear && printf '\e[3J'"
-alias dn='nvim ~/notes/daily-notes/(date +%Y-%m-%d).md'
 alias fixme='nvim ~/notes/fm/fixme.md'
 alias lg='lazygit'
 alias news='newsboat'
+alias dn='nvim ~/notes/daily-notes/(date +%Y-%m-%d).md'
+alias tn='nvim ~/notes/daily-notes/(date --date="tomorrow" +%Y-%m-%d).md'
+alias yn='nvim ~/notes/daily-notes/(date --date="yesterday" +%Y-%m-%d).md'
+alias uid='head -c30 /dev/urandom | base64 | tr -d "=/"'
+alias pandock='docker run --rm -v "$(pwd):/data" -u $(id -u):$(id -g) pandoc/extra'
+# alias mmut='mailsync.sh && mut'
+alias mmail='mail sync'
+alias jour='nvim ~/notes/journal'
+#alias man='if test (tput cols) -gt 80; set -gx MANWIDTH 80; else; set -e MANWIDTH; end && man'
 
 # git
 alias gss='git status -s'
 alias gd='git diff'
 alias gds='git diff --cached'
-alias gc='git commit -v'
+alias gc='git commit -sv'
 alias gapa='git add -p'
 alias gl='git pull'
 alias grv='git remote -v'
@@ -45,6 +82,10 @@ alias gcpr='git fetch upstream && checkout_pr '
 
 function checkout_pr -a pr
     git checkout pr/"$pr"
+    # extend this to reset changes of this branch to the commit where this branch started
+    # something like. this assumes branch main as parent
+    # set base $(git merge-base HEAD main)
+    # git reset "$base"
 end
 
 # Usage:
@@ -77,6 +118,16 @@ function reminder-stop-all
     end
 end
 
+function rebuild -a file -d "Watch the given Markdown file for modications and rebuild with pandoc template eisvogel"
+    set filestem (basename -s ".md" "$file")
+    set fileout "$filestem.pdf"
+    while true
+	inotifywait -e modify "$file" 2>/dev/null
+	echo "Rebuilding '$fileout'"
+	pandock --template eisvogel --listings -o "$fileout" "$file"
+    end
+end
+
 
 if command -v ip  > /dev/null
     alias ipa='ip -c addr'
@@ -103,14 +154,26 @@ end
 
 
 if command -v eza > /dev/null
-	alias l 'eza'
-	alias ll 'eza -lg'
-	alias la 'eza -lga --git'
+	abbr -a l 'eza'
+	abbr -a ll 'eza -lg --git'
+	abbr -a la 'eza -lag --git'
 else
-	alias l 'ls'
-	alias ll 'ls -l'
-	alias la 'ls -la'
+	abbr -a l 'ls'
+	abbr -a ll 'ls -l'
+	abbr -a la 'ls -la'
 end
+
+# email settings
+if command -v neomutt > /dev/null
+    abbr -a mutt 'neomutt'
+end
+
+# function mut
+#     alacritty msg config -w $ALACRITTY_WINDOW_ID  'font.normal = { family="Hermit" }'
+#     alacritty msg config -w $ALACRITTY_WINDOW_ID  'window.padding = { x = 12, y = 0 }'
+#     neomutt
+#     alacritty msg config -w $ALACRITTY_WINDOW_ID --reset
+# end
 
 
 # Type d to move up to top parent dir which is a repository
@@ -125,15 +188,18 @@ end
 
 
 # Environment variables
-set -gx MOAR "--style=gruvbox"
-set -gx PAGER moar
+#set -gx MOAR "--style=gruvbox"
+#set -gx PAGER moar
 set -gx EDITOR nvim
+set -gx DIFFPROG 'nvim -d'
+set -gx BROWSER firefox
 # X = leave content on-screen
 # F = quit automatically if less than one screenfull
 # R = raw terminal characters (fixes git diff)
 #     see http://jugglingbits.wordpress.com/2010/03/24/a-better-less-playing-nice-with-git/
-set -gx LESS '-R'
+set -gx LESS '-i -j .25 -R'
 set -gx DIRENV_LOG_FORMAT ''
+set -gx GPG_TTY (tty)
 
 # Fish git prompt
 set __fish_git_prompt_showuntrackedfiles 'yes'
@@ -143,6 +209,9 @@ set __fish_git_prompt_showupstream 'none'
 
 
 # Color manpage
+# 1=red, 2=green, 4=blue
+# 3=foreground, 4=background
+# 38,39 special cases
 # 30 = black
 # 31 = red
 # 32 = green
@@ -151,13 +220,18 @@ set __fish_git_prompt_showupstream 'none'
 # 35 = Magenta
 # 36 = cyan
 # 43 = yellow
-setenv LESS_TERMCAP_mb \e'[1;32m'               # begin blinking
-setenv LESS_TERMCAP_md \e'[1;32m'               # begin bold
-setenv LESS_TERMCAP_me \e'[0m'                  # end mode
-setenv LESS_TERMCAP_se \e'[0m'                  # end standout-mode
-setenv LESS_TERMCAP_so \e'[01;30;43m' # black text, yellow background. search results
-setenv LESS_TERMCAP_ue \e'[0m'                  # end underline
-setenv LESS_TERMCAP_us \e'[1;36m'               # begin underline
+
+# setenv LESS_TERMCAP_md \e'[1;32m' # start bold
+# setenv LESS_TERMCAP_me \e'[0m' # end bold
+# setenv LESS_TERMCAP_us \e'[1;36m' # start underline
+# setenv LESS_TERMCAP_ue \e'[0m' # end underline
+# setenv LESS_TERMCAP_so \e'[01;30;43m' # start stand out. (search) black text, yellow background.
+# setenv LESS_TERMCAP_se \e'[0m' # end stand out
+# setenv LESS_TERMCAP_mb \e'[1;32m' # begin blinking
+
+# function set_man_width
+#     set -x MANWIDTH (if test (tput cols) -gt 80; echo -n 80; else; echo -n 0; end)
+# end
 
 setenv MANWIDTH 80
 
@@ -175,21 +249,41 @@ function fish_prompt
 	echo -ns (set_color brblack) '['(date "+%H:%M")'] ' (set_color yellow) (prompt_pwd) (set_color green) (__fish_git_prompt) (set_color red) ' | '
 end
 
-function fish_greeting
-end
+# function fish_greeting
+# end
 
-[ -f /usr/share/autojump/autojump.fish ]; and source /usr/share/autojump/autojump.fish
+# [ -f /usr/share/autojump/autojump.fish ]; and source /usr/share/autojump/autojump.fish
+zoxide init fish | source
 
 # enable direnv
 direnv hook fish | source
 
 # only start ssh-agent if not running already
-if test -z (pgrep ssh-agent)
-    eval (ssh-agent -c | sed 's/^echo.*//')
-    # export vars accross all fish shells (-U)
-    set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
-    set -Ux SSH_AGENT_PID $SSH_AGENT_PID
-end
+# if test -z (pgrep ssh-agent)
+#     eval (ssh-agent -c | sed 's/^echo.*//')
+#     # export vars accross all fish shells (-U)
+#     set -gx SSH_AUTH_SOCK $SSH_AUTH_SOCK
+#     set -gx SSH_AGENT_PID $SSH_AGENT_PID
+# end
+
+#starship init fish | source
+set -x PATH "$PATH:$HOME/.cargo/bin"
+set -x PATH "$PATH:$HOME/.local/bin"
+set -x PATH "$PATH:$HOME/go/bin"
+set -x PATH "$PATH:$HOME/context/tex/texmf-linux-64/bin"
+
+# function syscall_nr
+#     cat /usr/src/linux/arch/x86/syscalls/syscall_64.tbl | \
+#     awk '$2 != "x32" && $3 == "'$1'" { print $1 }'
+# end
+
+# base16 colors
+# Base16 Shell
+# if status --is-interactive
+#     set BASE16_SHELL "$HOME/.config/base16-shell/"
+#     source "$BASE16_SHELL/profile_helper.fish"
+# end
+# base16_materia
 
 
 # enable vim keybindings. breaks Ctrl+f for autocompletion

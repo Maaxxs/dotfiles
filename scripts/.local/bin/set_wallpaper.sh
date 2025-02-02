a2a75e37bb405bb8ca3c2e7d593e01c754e2128e
Max Kunzelmann <maxdev@posteo.de>
2025-02-02 10:19:04 +0100

    x11: i3: merge configuration files with tower

--- scripts/.local/bin/set_wallpaper.sh
+++ scripts/.local/bin/set_wallpaper.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-if xrandr | grep '1440x2560' >/dev/null; then
+if xrandr | grep '1440x2560' >/dev/null
+then
     # detected a rotated monitor
+    # monitors go from left to right. vertical wallpaper first, then the other
+    #feh --bg-scale ~/Pictures/wallpapers/farn-vertical.jpg ~/Pictures/wallpapers/farn.jpg
     feh --bg-scale ~/Pictures/wallpapers/farn.jpg ~/Pictures/wallpapers/farn-vertical.jpg
 else
     feh --bg-scale ~/Pictures/wallpapers/farn.jpg

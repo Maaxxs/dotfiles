8e95b0abf7e9f04ffd1b917d2c838a9e573d3bff
Max Kunzelmann <maxdev@posteo.de>
2025-09-25 11:43:02 +0200

    script: format screenshot script. add quotes.

--- scripts/.local/bin/screenshot.sh
+++ scripts/.local/bin/screenshot.sh
@@ -13,26 +13,26 @@
 # bindsym Ctrl+Print        exec ~/.local/bin/screenshot.sh output
 # bindsym Ctrl+Shift+Print  exec ~/.local/bin/screenshot.sh all
 
-
 # region|window|output|all
 mode="$1"
 
-case $mode in 
-    "region")
-        grim -g "$(slurp)" - | swappy -f -
-        ;;
-    "window")
-        grim -g "$(swaymsg -t get_tree | jq -j '.. | select(.type?) | select(.focused).rect | "\(.x),\(.y) \(.width)x\(.height)"')" - | swappy -f -
-        ;;
-    "output")
-        grim -o $(swaymsg -t get_outputs | jq -r '.[] | select(.focused) | .name') - | swappy -f -
-        ;;
-    "all")
-        grim - | swappy -f -
-        ;;
-    *)
-        echo >&2 "unsupported command \"$mode\""
-        echo >&2 "Usage:"
-        echo >&2 "screenshot.sh <region|window|output|all>"
-        exit 1
+case $mode in
+"region")
+    grim -g "$(slurp)" - | swappy -f -
+    ;;
+"window")
+    grim -g "$(swaymsg -t get_tree | jq -j '.. | select(.type?) | select(.focused).rect | "\(.x),\(.y) \(.width)x\(.height)"')" - | swappy -f -
+    ;;
+"output")
+    grim -o "$(swaymsg -t get_outputs | jq -r '.[] | select(.focused) | .name')" - | swappy -f -
+    ;;
+"all")
+    grim - | swappy -f -
+    ;;
+*)
+    echo >&2 "unsupported command \"$mode\""
+    echo >&2 "Usage:"
+    echo >&2 "screenshot.sh <region|window|output|all>"
+    exit 1
+    ;;
 esac
